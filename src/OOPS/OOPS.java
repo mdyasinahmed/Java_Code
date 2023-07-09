d987505172d8e3f467f9843b61a7a8f755acd45a
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-09 23:50:56 +0600

    java oop  class practice code added

--- src/OOPS/OOPS.java
+++ src/OOPS/OOPS.java
@@ -8,17 +8,30 @@ import java.util.*;
         public void write(){
             System.out.println("Write Something.");
         }
+
+        public void printColor(){
+            System.out.println(this.color);
+        }
+
+        public void printType(){
+            System.out.println(this.type);
+        }
     }
 
 // Creating Object
 public class OOPS{
     public static void main(String args[]){
         Pen pen1 = new Pen();
-
         pen1.color = "blue";
         pen1.type = "gel";
-
         pen1.write();
+
+        Pen pen2 = new Pen();
+        pen2.color = "black";
+        pen2.type = "ballpoint";
+        
+        pen1.printColor();
+        pen2.printColor();
     }
 }
 
