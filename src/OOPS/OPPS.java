5bbbea416608dacc715a26bf2baed8c1e60900f5
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-09 10:26:17 +0600

    Java OOPS Classes and Objects code added

---
+++ src/OOPS/OPPS.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class OOPS{
+    // OOPS - Objects & Classes
+    public static void main(String args[]){
+        // Declaring a Class
+        /**
+         * Declaring class
+         * class ClassName{
+         * // fields - data
+         * // methods - function}
+         * 
+         * Declaring object
+         * public static void main(String args[]){
+         * }
+         */
+
+
+    }
+}
