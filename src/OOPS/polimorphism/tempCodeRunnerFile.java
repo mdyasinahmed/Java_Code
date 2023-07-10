bff5186c1d242f491658a7868383114713b4783f
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-10 22:55:13 +0600

    Polymorphism code bugs fixed

--- src/OOPS/polimorphism/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-    Student s1 = new Student;
-        s1.name = "aman";
-        s1.age = 24;
-
-        s1.printInfo(s1.name. s1.age);
--- src/OOPS/polymorphism/Polymorphism.java
+++ src/OOPS/polymorphism/Polymorphism.java
@@ -15,10 +15,12 @@ class Student{
     }
 }
 
-public static void main(String args[]){
-    Student s1 = new Student;
-    s1.name = "aman";
-    s1.age = 24;
+public class Polymorphism{
+    public static void main(String args[]){
+        Student s1 = new Student();
+        s1.name = "aman";
+        s1.age = 24;
 
-    s1.printInfo(s1.name. s1.age);
+        s1.printInfo(s1.name, s1.age);
+    }
 }
