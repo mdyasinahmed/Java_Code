7f8ff7532cbc98a823cc2f456f81b5d0a5b86230
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-10 09:33:31 +0600

    java creating constructor code added

--- src/OOPS/CONSTRACTORS.java
+++ src/OOPS/CONSTRACTORS.java
@@ -2,8 +2,23 @@
 // Declare
 /**
  * Class Test{
- *  Test(){
- *  // body
- * }
+ *      Test(){
+ *      // body
+ *      }
  * }
  */
+
+class Main{
+    private String name;
+    //Constructor
+    Main(){
+        System.out.println("Constructor Called.");
+        name = "fun coder";
+    }
+
+    public static void main(String args[]){
+        // constructor is invoked while creating object in main class
+        Main obj = new Main();
+        System.out.println("The name is" + obj.name);
+    }
+}
