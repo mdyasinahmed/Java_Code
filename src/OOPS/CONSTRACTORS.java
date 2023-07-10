addd7609b79c2c503e038a95b83da7f4c76a2860
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-10 09:48:01 +0600

    Non-Parametarized constructor code added

--- src/OOPS/CONSTRACTORS.java
+++ src/OOPS/CONSTRACTORS.java
@@ -15,10 +15,10 @@ class Main{
         System.out.println("Constructor Called.");
         name = "fun coder";
     }
+}
 
-    public static void main(String args[]){
+public static void main(String args[]){
         // constructor is invoked while creating object in main class
         Main obj = new Main();
         System.out.println("The name is" + obj.name);
-    }
 }
