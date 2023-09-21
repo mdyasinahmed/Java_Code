41f598cb1d6b4fdc3cd31d76c6b1fd2ea0a3bd58
mdyasinahmed <yasinahmed7875@gmail.com>
2023-09-21 23:42:06 +0600

    add public access modifier example code

---
+++ basic_oop/basic_oops/src/main/java/accessModifiers/defaultM/B.java
@@ -0,0 +1,13 @@
+package accessModifiers.defaultM;
+
+class A{
+    void msg() {
+        System.out.println("Hello");
+    }
+}
+public class B {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.msg();
+    }
+}
