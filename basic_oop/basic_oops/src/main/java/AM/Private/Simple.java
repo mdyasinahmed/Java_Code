1a3a64e73505ab07829fdf310d185960f3fe3102
mdyasinahmed <yasinahmed7875@gmail.com>
2023-09-22 21:36:23 +0600

    add access modifier example code

---
+++ basic_oop/basic_oops/src/main/java/AM/Private/Simple.java
@@ -0,0 +1,14 @@
+package AM.Private;
+
+class A{
+      void printHello(){
+        System.out.println("Hello World!");
+    }
+}
+public class Simple {
+    public static void main(String[] args){
+        A obj = new A();
+        obj.printHello();
+    }
+
+}
